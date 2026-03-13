adec56c20cc0a189eb8948796494477fd4a88a79
hbequet <206597685+hbequet@users.noreply.github.com>
2026-03-13 14:02:46 +0100

    continue implement routes

--- backend/src/controllers/movie.controller.js
+++ backend/src/controllers/movie.controller.js
@@ -8,7 +8,24 @@ export const getAllMovies = async (req, res, next) => {
     const limit = req.query.limit;
     const sortOption = req.query.sort;
 
+    const search = req.query.search;
+
     const skip = (page - 1) * limit;
+
+    if (search) {
+        const filter = {$or: [{title: {$regex: search, $options: 'i'}}, {description: {$regex: search, $options: 'i'}}]}
+        const movies = await Movie.find(filter)
+
+        const total = await Movie.countDocuments(filter)
+
+        return res.status(200).json({
+            "success": true,
+            "count": movies.length,
+            "total": total,
+            "movies": movies
+        })
+    }
+
     // Exécution de la requête
     const movies = await Movie.find(req.query)
         .sort(sortOption)
@@ -76,14 +93,43 @@ export const getSimilarMovies = async (req, res, next) => {
 // @route POST /api/movies
 // @access Private/Admin
 export const createMovie = async (req, res, next) => {
+    const { title, description, poster, backdrop, genre, year, duration, price, rating } = req.body;
 
+    const movie = await Movie.create({
+        title,
+        description,
+        poster,
+        backdrop,
+        genre,
+        year,
+        duration,
+        price,
+        rating
+    });
+
+    return res.status(201).json({
+        "success": true,
+        "data": movie
+    })
 }
 
 // @desc Modifier un film
 // @route PUT /api/movies/:id
 // @access Private/Admin
 export const updateMovie = async (req, res, next) => {
+    const updatedMovie = await Movie.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+            new: true,
+            runValidators: true
+        }
+    );
 
+    return res.status(200).json({
+        "success": true,
+        "data": updatedMovie
+    })
 }
 
 // @desc Supprimer un film
